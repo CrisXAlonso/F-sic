dd5ecac146370124c57b808d4e21960cfbdadd29
Cristian Alonso <CrisAlonso@dhcp-rhodes-1523.redrover.cornell.edu>
2016-02-20 15:51:53 -0500

    Initial Commit

---
+++ Füsic/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Füsic
+//
+//  Created by Cristian Alonso on 2/20/16.
+//  Copyright © 2016 Cristian Alonso. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
